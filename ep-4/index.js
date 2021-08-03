c1b8676c70fc1c8795e2a7d98c9f35446e5ccb73
gamebotjs <81354905+gamebotjs@users.noreply.github.com>
2021-08-03 20:08:24 +0530

    Update index.js

--- ep-4/index.js
+++ ep-4/index.js
@@ -1,1 +1,34 @@
+const Discord = require('discord.js')
+require("dotenv").config();
+const { Collection } = require('discord.js')
+const client = new Discord.Client();
+const fs = require('fs')
+client.commands = new Collection();
+client.queue = new Map();
 
+client.config = {
+    prefix: process.env.PREFIX
+}
+
+fs.readdir(__dirname + "/events/", (err, files) => {
+    if(err) return console.log(err);
+    files.forEach((file) => {
+        const event = require(__dirname + `/events/${file}`);
+        let eventName = file.split(".")[0];
+        client.on(eventName, event.bind(null, client));
+        console.log('Events loaded')
+    })
+});
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+    console.log(`Commands loaded!`)
+  });
+});
+
+client.login(process.env.TOKEN);
